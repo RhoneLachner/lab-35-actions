f23fdff85a1ef734dabfbefaa2295dcf307632cd
Rhone Lachner <fernandclay@gmail.com>
2021-02-17 14:40:02 -0800

    added file structure, elements on home page, and css

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import styles from './App.css';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <>
+    <div>
+      <a href="/">Home</a>
+      <h1>Posts</h1>
+    </div>
+
+    <section className={styles.inputSection}>
+      <h2>Create Post</h2>
+      <form>
+        <input name="title"></input>
+        <textarea name="body"></textarea>
+        <button>Submit ✓</button>
+      </form>
+    </section>
+
+    <section>
+      <ul></ul>
+    </section>
+  </>;
 }
