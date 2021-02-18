aada090f390dbac2d192b3563e88da61c81151ff
Rhone Lachner <fernandclay@gmail.com>
2021-02-17 16:33:14 -0800

    added delete reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { CREATE_POST } from '../actions/postActions';
+import { CREATE_POST, DELETE_POST } from '../actions/postActions';
 
 export const initialState = {
   posts:[]
@@ -11,6 +11,12 @@ export default function reducer(state, action) {
         ...state,
         posts: [...state.posts, action.payload]
       };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post.title !== action.payload)
+      };
+
     default:
       return state;
   }
